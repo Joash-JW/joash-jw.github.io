ba6a902560839a566ac2bd93d64cde9f8d957614
Joash <32333304+Joash-JW@users.noreply.github.com>
2019-11-24 17:23:25 -0500

    deploy

---
+++ precache-manifest.49ca11df2e690df389ae3600643d9aba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "383cdadcbf96b27c85cb",
+    "url": "/css/app.c2b9bd0e.css"
+  },
+  {
+    "revision": "383cdadcbf96b27c85cb",
+    "url": "/js/app.c627a894.js"
+  },
+  {
+    "revision": "1b720e3c2ad90fdfbc48",
+    "url": "/js/chunk-vendors.e0fc7273.js"
+  },
+  {
+    "revision": "0371c36f6aa7bcf37adf83d62d967ecc",
+    "url": "/img/seagate_CMYK_stacked_pos.0371c36f.png"
+  },
+  {
+    "revision": "0255bf2cd6e42c2fdbe2e189e418929f",
+    "url": "/img/github-icon.0255bf2c.svg"
+  },
+  {
+    "revision": "b2d61f43156ec29eb3795729c83f9bd6",
+    "url": "/img/in-square.b2d61f43.svg"
+  },
+  {
+    "revision": "42c868e993506a52183d852002c1e340",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
